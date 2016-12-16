d350876e6254217fb84590c0d59dcf34136025b1
HiiYL <yonglian146@gmail.com>
2016-12-16 14:33:19 +0800

    rockets now moved in slow motion

--- Assets/Missile/Scripts/MissileMover.cs
+++ Assets/Missile/Scripts/MissileMover.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MissileMover : MonoBehaviour {
     public float speed;
+//	public Vector3 emptyVector;
     // Use this for initialization
     void Start () {
-        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+//        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+//		emptyVector = new Vector3 (0, 0, 0);
     }
+	void Update() {
+//		GetComponent<Rigidbody> ().velocity = emptyVector;
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
 
 }
