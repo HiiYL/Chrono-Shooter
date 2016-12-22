cfe6864c8af6dd179aeb4a600d340af071c4e86a
HiiYL <yonglian146@gmail.com>
2016-12-22 16:25:41 +0800

    Added shapes, and tweaked lighting to have DYNAMIC LIGHTS

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	public GameObject obstacle;
+	public GameObject[] obstacles;
 	public GameObject player;
 
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 150; i++) {
-			GameObject obj = Instantiate (obstacle, player.transform.position
-				+ new Vector3 (Random.Range(-5,5), Random.Range(1,2), Random.Range(10,400)), Quaternion.identity);
+		int length = obstacles.Length;
+		for (int i = 0; i < 120; i++) {
+			GameObject obj = Instantiate (obstacles[Random.Range(0,length)], player.transform.position
+				+ new Vector3 (Random.Range(-5,5), Random.Range(1,3), Random.Range(10,400)), Quaternion.identity);
 			
-			obj.transform.localScale += new Vector3(Random.Range (0.5f, 1.5f),Random.Range (0.5f, 1.5f),Random.Range (0.5f, 1.5f));
+			obj.transform.localScale += new Vector3(Random.Range (0.5f, 1.5f),Random.Range (0.2f, 1.2f),Random.Range (0.5f, 1.5f));
 			
 		}
 	}
