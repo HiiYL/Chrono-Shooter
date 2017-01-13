476a0f6c98c5ce820efda284804065b0e3484947
HiiYL <yonglian146@gmail.com>
2017-01-13 11:45:15 +0800

    Added rotation to healthpots

---
+++ Assets/pickupRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.Rotate (0, 0, -34.883f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * 90f);
+		
+	}
+}
