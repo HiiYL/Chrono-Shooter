1bf201d37af902aebd54d3893569a8c03d440823
HiiYL <yonglian146@gmail.com>
2017-02-07 15:35:00 +0800

    Added chunked loading

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,11 +18,19 @@ public class GameManager : MonoBehaviour {
 
 	public float pauseCooldown = 0.3f;
 
+	public float chunkLoadDist = 5;
+	private int chunksLoaded = 0;
+
+	private float playerMoved = 0;
+	private float currentPlayerZCoord = 0;
+
 	// Use this for initialization
 	void Start () {
 		foreach (SpawnableObject obstacle in obstacles){
 			totalWeights += obstacle.spawnWeight;
 		}
+
+		currentPlayerZCoord = player.transform.position.z;
 
 		for (int i = 0; i < objectsToSpawn; i++) {
 			int selectedIdx = Random.Range (0, totalWeights);
@@ -32,7 +40,7 @@ public class GameManager : MonoBehaviour {
 				print (selectedIdx);
 				if (selectedIdx < 1) {
 					GameObject obj = Instantiate (obstacle.gameObj, player.transform.position
-						+ new Vector3 (Random.Range(-5,5), Random.Range(0,2), Random.Range(10,400)),
+						+ new Vector3 (Random.Range(-5,5), Random.Range(0,2), Random.Range(10,250)),
 						Quaternion.AngleAxis(90, Vector3.up));
 					break;
 				}
@@ -44,14 +52,39 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyUp (KeyCode.Escape)) {
 			print ("Key Pressed");
-			if(!pauseMenu.activeSelf)
-			{
+			if (!pauseMenu.activeSelf) {
 				Time.timeScale = 0;
-			} else{
+			} else {
 				Time.timeScale = 1;
 			}
 			pauseMenu.SetActive (!pauseMenu.activeSelf);
 		}
-
+		if (player.transform.position.z > currentPlayerZCoord) {
+			playerMoved += (player.transform.position.z - currentPlayerZCoord);
+			currentPlayerZCoord = player.transform.position.z;
+			chunkUpdateTrack ();
+		}
+	}
+	void chunkUpdateTrack(){
+		print ("Called Spawn Chunk!");
+		if ((playerMoved / chunkLoadDist) > chunksLoaded ) {
+			for (int i = 0; i < objectsToSpawn; i++) {
+				int selectedIdx = Random.Range (0, totalWeights);
+				print (selectedIdx);
+				foreach (SpawnableObject obstacle in obstacles) {
+					selectedIdx -= obstacle.spawnWeight;
+					if (selectedIdx < 1) {
+						GameObject obj = Instantiate (obstacle.gameObj, player.transform.position
+							+ new Vector3 (Random.Range(-5,5), Random.Range(0,2), Random.Range(10,250)),
+							Quaternion.AngleAxis(90, Vector3.up));
+						break;
+					}
+				}
+				//obj.transform.localScale += new Vector3(Random.Range (0.5f, 1.5f),Random.Range (0.2f, 1.2f),Random.Range (0.5f, 1.5f));
+			}
+			chunksLoaded = chunksLoaded + 1;
+		}
+		
+		
 	}
 }
